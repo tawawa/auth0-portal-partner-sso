b445245cdb2548891fbe87c5ce09bac2fa9f3cc2
Richard Seldon <arcseldon@gmail.com>
2018-04-16 18:03:48 +0900

    cleanup

--- partner-nodejs-regular-web-app/routes/other.js
+++ partner-nodejs-regular-web-app/routes/other.js
@@ -3,7 +3,7 @@ const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 const ensureSSOSession = require('../utils/ensureSSOSession');
 const router = express.Router();
 
-router.get('/', ensureSSOSession, function (req, res, next) {
+router.get('/', ensureSSOSession, ensureLoggedIn('/auth'), function (req, res, next) {
   res.render('other', { user: req.user });
 });
 
--- partner-nodejs-regular-web-app/routes/user.js
+++ partner-nodejs-regular-web-app/routes/user.js
@@ -3,9 +3,7 @@ const ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn;
 const ensureSSOSession = require('../utils/ensureSSOSession');
 const router = express.Router();
 
-/* GET user profile. */
-// router.get('/', ensureLoggedIn('/auth'), function (req, res, next) {
-router.get('/', ensureSSOSession, function (req, res, next) {
+router.get('/', ensureSSOSession, ensureLoggedIn('/auth'), function (req, res, next) {
   const env = {
     SESSION_URL: process.env.SESSION_URL
   };
